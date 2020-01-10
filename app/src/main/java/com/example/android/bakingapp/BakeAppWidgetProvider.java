71d207191c021b0a6464e15de4507aa4aa96938a
BassamMutairi <mutairibassam@gmail.com>
2020-01-10 22:23:29 +0300

    UPDATE: add widget

---
+++ app/src/main/java/com/example/android/bakingapp/BakeAppWidgetProvider.java
@@ -0,0 +1,57 @@
+package com.example.android.bakingapp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class BakeAppWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.bake_app_widget_provider);
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+        views.setTextViewText(R.id.appwidget_title, sharedPreferences.getString("WIDGET_TITLE", ""));
+        views.setTextViewText(R.id.appwidget_list, sharedPreferences.getString("WIDGET_LIST", ""));
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.appwidget_title, pendingIntent);
+        views.setOnClickPendingIntent(R.id.appwidget_list, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
--- app/src/main/java/com/example/android/bakingapp/DetailActivity.java
+++ app/src/main/java/com/example/android/bakingapp/DetailActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.bakingapp;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +16,21 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.android.bakingapp.fragment.RecipeStepsFragment;
 import com.example.android.bakingapp.model.BakingProcess;
+import com.example.android.bakingapp.model.Ingredients;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String TAG = "DetailActivity";
 
     private BakingProcess ingredients;
+    private long id;
+    private SharedPreferences sharedPreferences;
+    private LinearLayout linearLayout;
+    String recipeName;
+    List<Ingredients> listOfIngredients;
 
     private boolean mTwoPane;
 
@@ -23,7 +39,15 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_detail);
 
+        linearLayout = findViewById(R.id.recipe_detail);
+
+
             Intent intentCameFromBakingAdapter = getIntent();
+            ingredients = getIntent().getParcelableExtra(getString(R.string.ingredients_key));
+            listOfIngredients = ingredients.getIngredients();
+            id = ingredients.getId();
+            recipeName = ingredients.getName();
+
 
         if (findViewById(R.id.fragment_recipe_list_id) != null) {
 
@@ -46,7 +70,6 @@ public class DetailActivity extends AppCompatActivity {
 
                 } else {
                     mTwoPane = false;
-//                    Intent intentCameFromBakingAdapter = getIntent();
                     if (intentCameFromBakingAdapter.hasExtra(getString(R.string.ingredients_key))) {
                         ingredients = getIntent().getParcelableExtra(getString(R.string.ingredients_key));
                         RecipeStepsFragment recipeStepsFragment = new RecipeStepsFragment();
@@ -70,4 +93,73 @@ public class DetailActivity extends AppCompatActivity {
             }
         }
     }
+
+    private String ingredientToString() {
+        StringBuilder result = new StringBuilder();
+        for(Ingredients ingredients : listOfIngredients) {
+            result.append("- " + ingredients.getIngredient() + "\n");
+        }
+        return result.toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        // persistence.  Set checked state based on the fetchPopular boolean
+        sharedPreferences = getSharedPreferences(BuildConfig.APPLICATION_ID, MODE_PRIVATE);
+        if ((sharedPreferences.getInt("ID", -1) == id)){
+            menu.findItem(R.id.fav_widget).setIcon(R.drawable.ic_favorite_white_24dp);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.fav_widget){
+            boolean isRecipeInWidget = (sharedPreferences.getInt("ID", -1) == id);
+
+            // If recipe already in widget, remove it
+            if (isRecipeInWidget){
+                sharedPreferences.edit()
+                        .remove("ID")
+                        .remove("WIDGET_TITLE")
+                        .remove("WIDGET_LIST")
+                        .apply();
+
+                item.setIcon(R.drawable.ic_favorite_border_white_24dp);
+                Snackbar.make(linearLayout, "Widget Recipe Removed", Snackbar.LENGTH_SHORT).show();
+            }
+            // if recipe not in widget, then add it
+            else{
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    sharedPreferences
+                            .edit()
+                            .putInt("ID", Math.toIntExact(id))
+                            .putString("WIDGET_TITLE", recipeName)
+                            .putString("WIDGET_LIST",ingredientToString())
+                            .apply();
+                }
+
+                item.setIcon(R.drawable.ic_favorite_white_24dp);
+                Snackbar.make(linearLayout, "Widget Recipe Added", Snackbar.LENGTH_SHORT).show();
+            }
+
+            // Put changes on the Widget
+            ComponentName provider = new ComponentName(this, BakeAppWidgetProvider.class);
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+            int[] ids = appWidgetManager.getAppWidgetIds(provider);
+            BakeAppWidgetProvider bakingWidget = new BakeAppWidgetProvider();
+            bakingWidget.onUpdate(this, appWidgetManager, ids);
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+
 }
